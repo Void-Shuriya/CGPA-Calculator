cf1772e0882b57b2a07dc8ac2e9c8129bf1f70c3
Mystic Shun <mysticshun@gmail.com>
2024-12-22 13:57:51 +0800

    Added Text Files for Input and Output

--- CGPA Calculator/main.cpp
+++ CGPA Calculator/main.cpp
@@ -1,80 +1,194 @@
 #include <iostream>
+#include <fstream>
+#include <sstream> 
+#include <iomanip>
+#include <ctime> 
 
-void determineGrade(double cgpa) {
+using namespace std;
 
-	if (cgpa >= 0 && cgpa <= 1.99) {
-		std::cout << "You Have Failed!\n\n";
-	}
+struct studentRecords
+{
+    string name;
+    int asg1, asg2, test, quiz, exam;
+    double courseWork, overallScore;
+    char grade;
+};
 
-	else if (cgpa >= 2.00 && cgpa <= 2.49) {
-		std::cout << "You Have Passed! Your Grade is C!\n\n";
-	}
+// Counters for grades
+int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
 
-	else if (cgpa >= 2.50 && cgpa <= 2.99) {
-		std::cout << "Good Job! Your Grade Obtained is C+!\n\n";
-	}
+// Functions 
+void computeMarks(studentRecords& student);
+void computeGrade(const studentRecords& student);
+void studentResults(const studentRecords& student);
+void resultSummary(const studentRecords& student);
+void readStudentData();
 
-	else if (cgpa >= 3.00 && cgpa <= 3.49) {
-		std::cout << "Great! Your Grade Obtained is B!\n\n";
-	}
 
-	else if (cgpa >= 3.50 && cgpa <= 4.00) {
-		std::cout << "Excellent! Your Grade is A+!\n\n";
-	}
+void computeMarks(studentRecords& student)
+{
+    student.courseWork = ((student.asg1 / 100.00) * 0.25 + (student.asg2 / 100.00) 
+    * 0.25 + (student.test / 100.00) * 0.1 + (student.quiz / 100.00) * 0.1) * 70;
 
-	else {
-		std::cout << "You Have Entered an Invalid Grade!!\n\n";
-	}
+    student.overallScore = student.courseWork + ((student.exam / 100.0) * 30);
 }
 
-void viewGrade() {
-
-	std::cout << "\n\n";
-	std::cout << " GRADE   ||    CGPA      \n";
-	std::cout << "------------------------\n";
-	std::cout << "Grade A  -> 3.50 - 4.00\n";
-	std::cout << "Grade B  -> 3.00 - 3.49\n";
-	std::cout << "Grade C+ -> 2.50 - 2.99\n";
-	std::cout << "Grade C  -> 2.00 - 2.49\n";
-	std::cout << "Grade F  -> 0.00 - 1.99\n\n";
-
+void computeGrade(studentRecords& student)
+{
+    if (student.overallScore >= 70)
+    {
+        student.grade = 'A';
+        countA++;
+    }
+    else if (student.overallScore >= 60)
+    {
+        student.grade = 'B';
+        countB++;
+    }
+    else if (student.overallScore >= 50)
+    {
+        student.grade = 'C';
+        countC++;
+    }
+    else if (student.overallScore >= 40)
+    {
+        student.grade = 'D';
+        countD++;
+    }
+    else
+    {
+        student.grade = 'F';
+        countF++;
+    }
 }
 
-int main() {
+// Write to Results.txt
+void studentResults(const studentRecords& student)
+{
+    static bool header = false;
 
-	double cgpa;
-	int choice;
+    ofstream oFile("Results.txt", ios::app);
 
-	std::cout << "Welcome To The Results System\n\n";
+    if (oFile.is_open())
+    {
+        if (!header)
+        {
+            oFile << left
+                << setw(18) << "Name"
+                << setw(22) << "Coursework"
+                << setw(16) << "Exam"
+                << setw(22) << "Overall Score"
+                << setw(20) << "Grade" << endl
 
-	std::cout << "Menu Below\n";
-	std::cout << "------------\n";
-	std::cout << "1. View Grades\n";
-	std::cout << "2. Check Grade\n\n";
+                << string(85, '-') << endl;
 
-	std::cout << "Enter Your Option: ";
-	std::cin >> choice;
+            header = true;
+        }
+        oFile << left
+            << setw(20) << student.name
+            << setw(20) << fixed << setprecision(2) << student.courseWork
+            << setw(20) << fixed << setprecision(2) << static_cast<double>(student.exam)
+            << setw(20) << fixed << setprecision(2) << student.overallScore
+            << setw(20) << fixed << student.grade << endl;
 
-	if (choice == 1) {
+        oFile.close();
+    }
+    else
+    {
+        cerr << "Unable to write to Results File" << endl;
+    }
+}
 
-		viewGrade();
-	}
+void resultSummary(const studentRecords& student)
+{
+    ofstream sumFile("Results.txt", ios::app);
 
-	else if (choice == 2) {
+    if (sumFile.is_open())
+    {
+        // Get current date 
+        time_t now = time(0);
+        char dt[26];
+        ctime_s(dt, sizeof(dt), &now);
 
-		std::cout << "Enter Your CGPA: ";
-		std::cin >> cgpa;
-		std::cout << "\n";
+        sumFile
+            << "\n\n" << string(14, '-') << endl
+            << "Result Summary" << endl
+            << string(14, '-') << endl
+            << "Date: " << dt
+            << "Subject: Programming Methodology" << "\n\n"
+            << "Grade" << "\t" << "Student" << endl
+            << setw(3) << "A " << setw(8) << countA << endl
+            << setw(3) << "B " << setw(8) << countB << endl
+            << setw(3) << "C " << setw(8) << countC << endl
+            << setw(3) << "D " << setw(8) << countD << endl
+            << setw(3) << "F " << setw(8) << countF << endl
+            << endl << "Total Students: " << countA + countB + countC + countD + countF;
+    }
+    else
+    {
+        cerr << "Unable to write to Results File" << endl;
+    }
+}
 
-		determineGrade(cgpa);
-	}
+void readStudentData()
+{
+    studentRecords student;
 
-	else {
+    string line;
+    ifstream inFile("Student.txt");
 
-		std::cout << "Please Enter only 1 or 2!!\n";
-	}
+    if (inFile.is_open())
+    {
+        // Header
+        cout << endl << "Name " << setw(17) << "Asg1"
+            << setw(9) << "Asg2" << setw(9) << "Test"
+            << setw(9) << "Quiz" << setw(9) << "Exam" << "\n";
 
-	std::cout << "This is the End of the System!\n\n";
+        cout << string(60, '-') << endl;
 
-	return 0;
+        while (getline(inFile, line)) {
+            istringstream iss(line);
+
+            // Extract the name 
+            student.name = "";
+            char ch;
+            while (iss.get(ch) && !isdigit(ch)) {
+                student.name += ch;
+            }
+
+            // Scores after name
+            iss.putback(ch);
+            iss >> student.asg1 >> student.asg2 >> student.test
+                >> student.quiz >> student.exam;
+
+            // Write all to Results.txt File
+            computeMarks(student);
+            computeGrade(student);
+            studentResults(student);
+
+            // Display Student data In Console
+            cout << left << setw(18) << student.name
+                << setw(9) << fixed << setprecision(2) << static_cast<double>(student.asg1)
+                << setw(9) << fixed << setprecision(2) << static_cast<double>(student.asg2)
+                << setw(9) << fixed << setprecision(2) << static_cast<double>(student.test)
+                << setw(9) << fixed << setprecision(2) << static_cast<double>(student.quiz)
+                << setw(9) << fixed << setprecision(2) << static_cast<double>(student.exam) << "\n";
+        }
+
+        inFile.close();
+        resultSummary(student);
+    }
+    else
+    {
+        cerr << "Error: Unable to open file." << endl;
+        return;
+    }
+}
+
+// Main Function
+int main()
+{
+    readStudentData();
+    cout << endl;
+    return 0;
 }
